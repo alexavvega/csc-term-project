5cdfaa2fceeb9b4e120bbf383e0bd301f90995e2
KGrayH <107825348+KGrayH@users.noreply.github.com>
2025-05-22 13:33:04 -0700

    updates
    updates, mobile-friendly

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+const authRoutes = require('./routes/auth');
+const productRoutes = require('./routes/products');
+const userRoutes = require('./routes/users');
+const cartRoutes = require('./routes/cart');
+
+app.set('view engine', 'ejs'); // or pug, hbs, etc.
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/battleToads', (req, res) => {
+  res.render('battleToads'); // no .html if using EJS
+});
+
+app.use('/auth', authRoutes);
+app.use('/products', productRoutes);
+app.use('/users', userRoutes);
+app.use('/cart', cartRoutes);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.get('/product', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/product.html'));
+});
+
+app.get('/cart', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/cart.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/about.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/login.html'));
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/register.html'));
+});
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
